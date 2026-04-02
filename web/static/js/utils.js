e811338beaada16868e847928682f39b46503e9a
Kh4ht <kingkh4led@yahoo.com>
2026-04-02 15:44:36 +0300

    added search in results, and style changes.

---
+++ web/static/js/utils.js
@@ -0,0 +1,36 @@
+function emptyTextInput(inputElement)
+{
+    if (inputElement & inputElement.value !== undefined)
+    {
+        inputElement.value = '';
+    }
+    else
+    {
+        console.error('Invalid input element provided !')
+    }
+}
+
+function emptyTextInputById(inputId)
+{
+    const inputElement = document.getElementById(inputId);
+
+    if (inputElement)
+    {
+        inputElement.value = '';
+    }
+    else
+    {
+        console.error(`Input element with ID ${inputId} not found`)
+    }
+}
+
+function emptyMultipleInputs(...inputElements)
+{
+    inputElements.forEach(input => 
+    {
+        if (input && input.value !== undefined)
+        {
+            input.value = '';
+        }
+    });
+}
